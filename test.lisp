bd807886978fa1b18da98a1c71c39ad825a41733
Luke Horgan <horgan.l@husky.neu.edu>
2019-01-16 12:31:23 -0500

    stricter typing stuff

--- test.lisp
+++ test.lisp
@@ -27,65 +27,71 @@
 
 ; ******************* END INITIALIZATION FOR ACL2s MODE ******************* ;
 ;$ACL2s-SMode$;ACL2s
-;(include-book "std/alists/top" :dir :system)
-
-#|(defmacro lenp (delta n)
-  (and (natp n) (listp delta) (>= (len delta) n))
-  )|#
-
-(defun tao-listp (tl)
+#|(defun tau-listp (tl)
   (cond ((integerp tl) T)
         ((equal tl nil) T)
-        ((true-listp tl) (and (tao-listp (first tl)) (tao-listp (rest tl))))
-        )
-  )
-
-#|(defmacro tao-listp (tl)
-  (list 'cond (list (list 'integerp tl) T)
-              (list (list 'endp tl) T)
-              (list (list 'listp tl) (list 'and (list 'tao-listp (list 'first tl)) (list 'tao-listp (list 'rest tl))))))|#
-
-#|(defunc tao-listp (tl)
-  :input-contract (or (integerp tl) (endp tl) (consp tl))
-  :output-contract (boolean p (tao-listp tl))
-  (cond ((integerp tl) T)
-        ((endp tl) T)
-        ((consp tl) (and (tao-listp (first tl)) (tao-listp (second tl))))
+        ((true-listp tl) (and (tau-listp (first tl)) (tau-listp (rest tl))))
         )
   )|#
+
+#|(defmacro tau-listp (tl)
+  (list 'cond (list (list 'integerp tl) T)
+              (list (list 'endp tl) T)
+              (list (list 'listp tl) (list 'and (list 'tau-listp (list 'first tl)) (list 'tau-listp (list 'rest tl))))))|#
+
+#|(skip-proofs
+(defunc tau-listp2 (tl)
+  :input-contract (or (integerp tl) (true-listp tl))
+  :output-contract (booleanp (tau-listp2 tl))
+  (cond ((integerp tl) T)
+        ((equal tl nil) T)
+        ((true-listp tl) (and (tau-listp2 (first tl)) (tau-listp2 (rest tl))))
+        )
+  )
+)|#
+
+;(skip-proofs
+(defunc tau-listp (tl)
+  :input-contract T
+  :output-contract (booleanp (tau-listp tl))
+  (cond ((integerp tl) T)
+        ((equal tl nil) T)
+        ((true-listp tl) (and (tau-listp (first tl)) (tau-listp (rest tl))))
+        (T nil)
+        )
+  )
+;)
+
+(defthm typing
+  (implies (tau-listp x) (or (true-listp x) (integerp x)))
+  )
 
 (defmacro lenp (delta n)
   (list 'and (list 'natp n) (list 'true-listp delta) (list '>= (list 'len delta) n))
   )
 
-#|(defunc lenp_check (delta n)
-  :input-contract (and (tao-listp delta) (natp n) (>= (len delta) n))
-  :output-contract (booleanp (lenp_check delta n))
-  (lenp delta n)
-  )|#
-
 (defunc i_return (delta)
-   :input-contract (and (listp delta) (integerp (first delta)))
+   :input-contract (and (true-listp delta) (tau-listp delta) (integerp (first delta)))
    :output-contract (integerp (first delta))
    (first delta)
    )
 
-(defunc taop (a)
+#|(defunc taup (a)
   :input-contract a
-  :output-contract (booleanp (taop a))
+  :output-contract (booleanp (taup a))
   (or (natp a) (listp a))
-  )
+  )|#
 
 
 (defunc i_dup (delta)
-  :input-contract (listp delta)
-  :output-contract (listp (i_dup delta))
+  :input-contract (and (tau-listp delta) (true-listp delta))
+  :output-contract (tau-listp (i_dup delta))
   (cons (first delta) delta)
   )
 
 (defunc i_const (n delta)
-  :input-contract (listp delta)
-  :output-contract (listp (i_const n delta))
+  :input-contract (and (tau-listp delta) (true-listp delta) (tau-listp n))
+  :output-contract (tau-listp (i_const n delta))
   (cons n delta)
   )
 
@@ -99,36 +105,38 @@
   )
 
 (defunc i_pop (delta)
-  :input-contract (and (true-listp delta) (lenp delta 1))
-  :output-contract (true-listp (i_pop delta))
+  :input-contract (and (true-listp delta) (tau-listp delta) (lenp delta 1))
+  :output-contract (and (true-listp (i_pop delta)) (tau-listp delta))
   (rest delta)
   )
 
 (defunc i_add (delta)
-  :input-contract (and (lenp delta 2) (integerp (first delta)) (integerp (second delta)))
+  :input-contract (and (lenp delta 2) (tau-listp delta) (integerp (first delta)) (integerp (second delta)))
   :output-contract (and (true-listp (i_add delta)) (integerp (first (i_add delta))))
   (cons (+ (first delta) (second delta)) (i_pop (i_pop delta)))
   )
 
 (defunc i_swap (delta)
-  :input-contract (lenp delta 2)
-  :output-contract (listp (i_swap delta))
+  :input-contract (and (tau-listp delta) (lenp delta 2))
+  :output-contract (and (tau-listp (i_swap delta)) (true-listp delta))
   (cons (second delta) (cons (first delta) (rest (rest delta))))
   )
 
 (defthm nil-if-pop-twice-from-list-of-two
-  (implies (and (true-listp x) (equal (len x) 2)) (equal (i_pop (i_pop x)) nil))
+  (implies (and (true-listp x) (tau-listp x) (equal (len x) 2)) (equal (i_pop (i_pop x)) nil))
   )
 
 (defunc i_mul (delta)
   :input-contract (and 
                    (true-listp delta) 
+                   (tau-listp delta)
                    (lenp delta 2) 
                    (integerp (first delta)) 
                    (integerp (second delta))
                    )
   :output-contract (and
                     (true-listp (i_mul delta))
+                    (tau-listp (i_mul delta))
                     (integerp (first (i_mul delta))))
   :function-contract-hints (nil-if-pop-twice-from-list-of-two)
   (cons (* (first delta) (second delta)) (i_pop (i_pop delta)))
@@ -136,7 +144,8 @@
 
 (defunc i_div (delta)
   :input-contract (and 
-                   (true-listp delta) 
+                   (true-listp delta)
+                   (tau-listp delta)
                    (lenp delta 2) 
                    (integerp (first delta)) 
                    (integerp (second delta))
@@ -144,13 +153,15 @@
                    )
   :output-contract (and
                     (true-listp (i_div delta))
+                    (tau-listp (i_div delta))
                     (integerp (first (i_div delta))))
   (cons (floor (/ (first delta) (second delta)) 1) (i_pop (i_pop delta)))
   )
 
 (defunc i_mod (delta)
   :input-contract (and 
-                   (true-listp delta) 
+                   (true-listp delta)
+                   (tau-listp delta)
                    (lenp delta 2) 
                    (integerp (first delta)) 
                    (integerp (second delta))
@@ -158,60 +169,38 @@
                    )
   :output-contract (and
                     (true-listp (i_mod delta))
+                    (tau-listp (i_mod delta))
                     (integerp (first (i_mod delta))))
   (cons (mod (first delta) (second delta)) (i_pop (i_pop delta)))
-  )
-
-(defunc i_store (delta mu)
-  :input-contract (and
-                   (true-listp delta)
-                   (lenp delta 2)
-                   (true-listp mu)
-                   (integerp (first delta))
-                   )
-  :output-contract (and
-                    (equal (first delta) (first (first (second (i_store delta mu)))))
-                    (equal (second delta) (second (first (second (i_store delta mu)))))
-                    (equal (len (first (i_store delta mu))) (- (len delta) 2))
-                    )
-  (list (i_pop (i_pop delta)) 
-        (cons (list (first delta) (second delta)) mu))
   )
 
 (defunc is_a_list (list)
   :input-contract (true-listp list)
   :output-contract (booleanp (is_a_list list))
   (cond ((equal list nil) T)
-        ((and (true-listp (first list)) (atom-listp (first list)) (equal (len (first list)) 2) (is_a_list (rest list))) T)
+        ((and (true-listp (first list)) #|(atom-listp (first list))|# (equal (len (first list)) 2) (is_a_list (rest list))) T)
         (T nil))
   )
 
-#|(defunc has_key (list key)
-  :input-contract (and (true-listp list) (is_a_list list))
-  :output-contract (booleanp (has_key list key))
-  (if (equal list nil) nil (or (equal (first (first list)) key) (has_key (rest list) key)))
+(defunc i_store (delta mu)
+  :input-contract (and
+                   (true-listp delta)
+                   (lenp delta 2)
+                   (tau-listp delta)
+                   (integerp (first delta))
+                   (true-listp mu)
+                   (is_a_list mu)
+                   )
+  :output-contract (and
+                    ;(equal (first delta) (first (first (second (i_store delta mu)))))
+                    ;(equal (second delta) (second (first (second (i_store delta mu)))))
+                    ;(equal (len (first (i_store delta mu))) (- (len delta) 2))
+                    (is_a_list (second (i_store delta mu)))
+                    (tau-listp (first (i_store delta mu)))
+                    )
+  (list (i_pop (i_pop delta)) 
+        (cons (list (first delta) (second delta)) mu))
   )
-
-(defunc still_a_list (list)
-  :input-contract (and (true-listp list) (is_a_list list) (consp list))
-  :output-contract (is_a_list (still_a_list list))
-  (rest list)
-  )
-
-(defunc add_to_a_list (entry list)
-  :input-contract (and (true-listp entry) (equal (len entry) 2) (is_a_list entry) (true-listp list) (is_a_list list))
-  :output-contract (and (true-listp (add_to_a_list entry list)) (is_a_list (add_to_a_list entry list)))
-  (append entry list)
-  )|#
-
-#|(defunc integer-alistp (list)
-  :input-contract (and (true-listp list) (is_a_list list))
-  :output-contract (booleanp (integer-alistp list))
-  (cond ((equal list nil) T)
-        ((not (and (integerp (first (first list))) (integerp (second (first list))))) nil)
-        (T (integer-alistp (rest list)))
-        )
-  )|#
   
 (defunc get_val (mu key)
   :input-contract (and (true-listp mu)  (is_a_list mu) (integerp key))
@@ -220,21 +209,7 @@
         ((equal (first (first mu)) key) (second (first mu)))
         (T (get_val (rest mu) key))
         )
-  )
-
-#|((MU '((-1/4 -1) (NIL NIL))) (DELTA '(-11 NIL)))|#
-
-#|(defunc make_list (thing)
-  :input-contract T
-  :output-contract (listp (make_list thing))
-  (if (listp thing) thing '(thing))
-  )|#
-
-(defun x4y4 (x y)
-  (let ((x2 (* x x))            ; Let x2 be x^2 and, ``simultaneously,''
-        (y2 (* y y)))           ; let y2 be y^2.
-    (+ (* x2 x2)                ; Compute x^4 by squaring x^2 
-       (* y2 y2))))            ; and add it y^4 computed similarly.
+  )#|ACL2s-ToDo-Line|#
 
 
 (defunc i_load (delta mu)
@@ -243,13 +218,11 @@
                    (true-listp mu)
                    (is_a_list mu)
                    (true-listp delta)
+                   (tau-listp delta)
                    (lenp delta 1)
                    (integerp (first delta))
                    )
   :output-contract (true-listp (i_load delta mu))
-  ;(let ((thingie (if (true-listp (first delta)) (first delta) '(first delta))))
   (let ((val (get_val mu (first delta))))
   (append (if (true-listp val) val (list val)) (i_pop delta)))
-   ;(list mu (get_val mu (first delta)) (i_pop delta))
-  )#|ACL2s-ToDo-Line|#
-
+  )
